644e22357be5f44c83dd43343260241955cfaf5a
Parth <Parthpurbia@gmail.com>
2025-05-23 13:23:33 +0530

    Modification

--- user_logger .py
+++ user_logger .py
@@ -1,7 +1,15 @@
+# user_logger.py
+
 import sys
 
+# Get command line arguments
 username = sys.argv[1]
 email = sys.argv[2]
 
-print(f"Username: {username}")
-print(f"Email: {email}")
+# Print or process them
+print(f"Received Username: {username}")
+print(f"Received Email: {email}")
+
+# Optionally write to a file
+with open("user_log.txt", "a") as f:
+    f.write(f"Username: {username}, Email: {email}\n")
